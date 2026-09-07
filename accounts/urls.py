53157e95c07519772cda2935ab167bef0ba5c1f8
Alade Zaccheous <aladezaccheous52@gmail.com>
2026-09-07 14:10:03 +0100

    update password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,33 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
     path('redirect/', views.role_redirect_view, name='role_redirect'),
     path('register/', views.register_patient, name='register'),
+
+       # Password Reset URLs
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             subject_template_name='accounts/password_reset_subject.txt',
+             success_url='/accounts/password-reset/done/'
+         ),
+         name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html',
+             success_url='/accounts/login/'
+         ),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
